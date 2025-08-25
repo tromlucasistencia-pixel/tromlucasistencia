51a01506b5042ddafa893a04a04e6eb54626ebd3
Trom Luc <tromlucasistencia@gmail.com>
2025-08-25 13:05:53 -0600

    otro cambio en app.py

--- app.py
+++ app.py
@@ -383,8 +383,9 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    pass  # o simplemente comenta toda esta sección
-    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))  # ✅ toma PORT de Railway o usa 5000 local
+    app.run(host="0.0.0.0", port=port)
+
 
 
 

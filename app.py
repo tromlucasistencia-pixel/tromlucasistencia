6aae8da12eb3d1946f196ed43b8e4332ddfb4295
Alan Soto <alansoto121298@gmail.com>
2025-09-04 10:16:52 -0600

    edit pintura

--- app.py
+++ app.py
@@ -618,10 +618,10 @@ def registrar_asistencia():
         aires_lon_max = -101.23635
 
         # Zona PINTURA
-        pintura_lat_min = 20.6095
-        pintura_lat_max = 20.6110
-        pintura_lon_min = -101.2395
-        pintura_lon_max = -101.2375
+        pintura_lat_min = 20.60988
+        pintura_lat_max = 20.61006
+        pintura_lon_min = -101.23931
+        pintura_lon_max = -101.23912
 
         if aires_lat_min <= lat <= aires_lat_max and aires_lon_min <= lon <= aires_lon_max:
             ubicacion = "Ubicación en zona AIRES"
